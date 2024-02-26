e13c9a86bf75abfa371c6e415df14d03c4448275
Simon Duflot <narcisse@gmail.com>
2024-02-26 13:33:30 +0100

    thunder client test ok

--- video_processing_service/src/index.ts
+++ video_processing_service/src/index.ts
@@ -1,12 +1,42 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World !");
+// app.get("/", (req, res) => {
+//     res.send("Hello World !");
+// });
+
+app.post("/process-video", (req, res) => {
+    // Récupérer le chemin de la vidéo à convertir et la donner au body de la request
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath){
+        let missingPaths = [];
+        if (!inputFilePath){
+            missingPaths.push(`${inputFilePath}`);
+        }
+        if (!outputFilePath){
+            missingPaths.push(`${outputFilePath}`);
+        }
+        res.status(400).send(`Bad Request: Missing file path => ${missingPaths.join()}` );
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", function() {
+            res.status(200).send("Video processing finished successfully.");
+        })
+        .on("error", function(err: any) {
+            console.log(`An error orrcured: ${err.message}`); 
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
     console.log(
         `Video processing service listening at http://localhost:${port}`)
