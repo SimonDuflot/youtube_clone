1fba406edf8e84637cab7dad891be9f8db6ea2c0
Simon Duflot <narcisse@gmail.com>
2024-02-28 23:47:31 +0100

    download process upload delete - video functions

--- video_processing_service/src/storage.ts
+++ video_processing_service/src/storage.ts
@@ -1,10 +1,10 @@
 // 1. Google Cloud Storage interactions des fichiers
 // Créer deux buckets GCS : un où on peut upload une vidéo, puis un autre où les vidéos uploader seront "processed"
-// Vider les repertoires raw et processed une fois la vidéo "processed"
+// Vider les repertoires raw et processed une fois la vidéo traitée
 // On a besoin que notre video_processing_service puisse:
 //          - télécharger une vidéo depuis GCS
 //          - traiter la vidéo
-//          - publier la vidéo tratée dans GCS
+//          - publier la vidéo traitée dans GCS
 
 import { Storage } from "@google-cloud/storage";
 import fs from "fs";
@@ -29,7 +29,7 @@ export function setupDirectories() {
  * @returns - Une promesse résolue dès que la vidéo est convertie
  */
 export function convertVideo(rawVideoName: string, processedVideoName: string) {
-    return new Promise (resolve, reject) => {
+    return new Promise<void> ((resolve, reject) => {
         ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
             .outputOptions("-vf", "scale=-1:360") // 360p
             .on("end", function() {
@@ -38,7 +38,65 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
             })
             .on("error", function(err: any) {
                 console.log(`An error orrcured: ${err.message}`); 
+                reject(err);
             })
             .save(`${localProcessedVideoPath}/${processedVideoName}`);
-    }
+    })
+}
+
+/**
+ * @param fileName - Le nom du fichier à télécharger depuis 
+ * {@link rawVideoBucketName} à l'intéreur de 
+ * {@link localRawVideoPath}.
+ * @returns - Une promesse résolue quand le fichier est téléchargé 
+ */
+export async function downloadRawVideo(fileName: string) {
+    await storage.bucket(rawVideoBucketName)
+        .file(fileName)
+        .download({ destination: `${localRawVideoPath}/${fileName}`});
+    
+    console.log(
+        `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}`
+    )
+}
+
+/**
+ * @param fileName - Le nom du fichier à transférer depuis 
+ * {@link localProcessedVideoPath} à l'intéreur de 
+ * {@link processedVideoBucketName}.
+ * @returns - Une promesse résolue quand le fichier est transféré 
+ */
+export async function uploadProcessedVideo (fileName: string) {
+    const bucket = storage.bucket(processedVideoBucketName);
+
+    await bucket.upload(`${localProcessedVideoPath}/${fileName}`, {
+        destination: fileName
+    });
+
+    await bucket.file(fileName).makePublic();
+
+}
+
+/**
+ * @param filePath - Le chemin contenant le fichier à détruire
+ * @returns - Une promesse réqolue quand le fichier est détruit
+ */
+function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log(`Failed to delete file at ${filePath}`, err);
+                    reject(err);
+                } else {
+                    console.log(`File deleted at ${filePath}`)
+                    resolve();
+                }
+            });
+        } else {
+            console.log(`File not found at ${filePath}, skipping the delete.`);
+            // reject ou resolve ?
+            resolve();
+        }
+    });
 }
